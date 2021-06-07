34b3949108f988331f6a10cae00c4f1ca79f0f00
huangjianwei <huangjianwei@hba.cn>
2021-06-07 10:50:08 +0800

    Initial Commit

---
+++ CppTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppTest
+//
+//  Created by rehab on 2021/6/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
